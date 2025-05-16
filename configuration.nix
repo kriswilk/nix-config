85e1a54402cd28ee939b5ab378fe40d47f727014
Kris Wilk <kris@reefnet.ca>
2025-05-16 11:20:33 -0400

    start ssh-agent on login

--- configuration.nix
+++ configuration.nix
@@ -105,6 +105,8 @@
   # Enable the OpenSSH daemon.
   # services.openssh.enable = true;
 
+  programs.ssh.startAgent = true;
+
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
   # networking.firewall.allowedUDPPorts = [ ... ];
