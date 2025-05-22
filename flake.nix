358945c66ba7e2debefd8abc098b7f4f064b4f8b
Kris Wilk <kris@reefnet.ca>
2025-05-22 16:29:26 -0400

    Add disko to flake.nix

--- flake.nix
+++ flake.nix
@@ -3,14 +3,17 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    disko.url = "github:nix-community/disko/latest";
+    disko.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, ... }@inputs: {
+  outputs = { self, nixpkgs, disko, ... }@inputs: {
     nixosConfigurations = {
       vm-nixos = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
           ./hosts/vm-nixos/configuration.nix
+          disko.nixosModules.disko
         ];
       };
     };
