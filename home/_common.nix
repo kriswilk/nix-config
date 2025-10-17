8628a8ef69443179b97f41048c5b638a7843c66f
Kris Wilk <kris@reefnet.ca>
2025-10-17 14:26:45 -0400

    WIP: Minimal packages for testing disko-install

--- home/_common.nix
+++ home/_common.nix
@@ -17,187 +17,185 @@
     # crystaldiskmark
 
     # misc
-    kdePackages.krunner # already installed?
-    kdePackages.kalk # already installed?
-    kdePackages.kcalc # already installed?
+    #kdePackages.krunner # already installed?
+    #kdePackages.kalk # already installed?
+    #kdePackages.kcalc # already installed?
 
     # suites
-    libreoffice
+    #libreoffice
 
     # disk / file
-    kdePackages.ark # already installed?
-    kdePackages.filelight # already installed?
-    kdePackages.k3b # already installed?
-    krename # anything better?
-    bcompare
-    ventoy
-    qbittorrent
+    #kdePackages.ark # already installed?
+    #kdePackages.filelight # already installed?
+    #kdePackages.k3b # already installed?
+    #krename # anything better?
+    #bcompare
+    #ventoy #insecure warning?
+    #qbittorrent
 
     # audio
-    audacity
+    #audacity
 
     # video
-    vlc
-    ffmpeg
-    handbrake
-    mkvtoolnix
-    stremio
+    #vlc
+    #ffmpeg
+    #handbrake
+    #mkvtoolnix
+    #stremio
 
     # photo / illustration
-    gimp3
-    inkscape
-    krita
+    #gimp3
+    #inkscape
+    #krita
 
     # 3d / cad
-    blender
-    freecad
-    kicad
-    prusa-slicer
+    #blender
+    #freecad
+    #kicad
+    #prusa-slicer
 
     # text
-    kdePackages.kate # already installed?
-    kdePackages.okular # already installed?
-    calibre
-    koreader
+    #kdePackages.kate # already installed?
+    #kdePackages.okular # already installed?
+    #calibre
+    #koreader
 
     # print / scan
-    naps2
+    #naps2
 
     # programming
-    vscode-fhs
+    #vscode-fhs
     #vscode
-    imhex
+    #imhex
 
     # networking
-    tailscale
-    wireshark
-    angryipscanner
-    kdePackages.krdc # already installed?
+    #tailscale
+    #wireshark
+    #angryipscanner
+    #kdePackages.krdc # already installed?
 
     # storage
-    rclone
+    #rclone
 
     # gaming
-    jdk
-    prismlauncher
+    #jdk
+    #prismlauncher
 
     # cli
-    neofetch
+    fastfetch
 
 
 
 
+    # UNSORTED ITEMS FROM OTHER FLAKES
 
+    # nnn # a fast and lightweight terminal file manager
 
+    # # archives
+    # zip
+    # xz
+    # unzip
+    # p7zip
 
+    # # utils
+    # ripgrep # recursively searches directories for a regex pattern
+    # jq # A lightweight and flexible command-line JSON processor
+    # yq-go # yaml processor https://github.com/mikefarah/yq
+    # eza # A modern replacement for ‘ls’
+    # fzf # A command-line fuzzy finder
 
-    nnn # a fast and lightweight terminal file manager
+    # # networking tools
+    # mtr # A network diagnostic tool
+    # iperf3
+    # dnsutils  # `dig` + `nslookup`
+    # ldns # replacement of `dig`, it provide the command `drill`
+    # aria2 # A lightweight multi-protocol & multi-source command-line download utility
+    # socat # replacement of openbsd-netcat
+    # nmap # A utility for network discovery and security auditing
+    # ipcalc  # it is a calculator for the IPv4/v6 addresses
 
-    # archives
-    zip
-    xz
-    unzip
-    p7zip
+    # # misc
+    # cowsay
+    # file
+    # which
+    # tree
+    # gnused
+    # gnutar
+    # gawk
+    # zstd
+    # gnupg
 
-    # utils
-    ripgrep # recursively searches directories for a regex pattern
-    jq # A lightweight and flexible command-line JSON processor
-    yq-go # yaml processor https://github.com/mikefarah/yq
-    eza # A modern replacement for ‘ls’
-    fzf # A command-line fuzzy finder
+    # # productivity
+    # hugo # static site generator
+    # glow # markdown previewer in terminal
 
-    # networking tools
-    mtr # A network diagnostic tool
-    iperf3
-    dnsutils  # `dig` + `nslookup`
-    ldns # replacement of `dig`, it provide the command `drill`
-    aria2 # A lightweight multi-protocol & multi-source command-line download utility
-    socat # replacement of openbsd-netcat
-    nmap # A utility for network discovery and security auditing
-    ipcalc  # it is a calculator for the IPv4/v6 addresses
+    # btop  # replacement of htop/nmon
+    # iotop # io monitoring
+    # iftop # network monitoring
 
-    # misc
-    cowsay
-    file
-    which
-    tree
-    gnused
-    gnutar
-    gawk
-    zstd
-    gnupg
+    # # system call monitoring
+    # strace # system call monitoring
+    # ltrace # library call monitoring
+    # lsof # list open files
 
-    # productivity
-    hugo # static site generator
-    glow # markdown previewer in terminal
-
-    btop  # replacement of htop/nmon
-    iotop # io monitoring
-    iftop # network monitoring
-
-    # system call monitoring
-    strace # system call monitoring
-    ltrace # library call monitoring
-    lsof # list open files
-
-    # system tools
-    sysstat
-    lm_sensors # for `sensors` command
-    ethtool
-    pciutils # lspci
-    usbutils # lsusb
+    # # system tools
+    # sysstat
+    # lm_sensors # for `sensors` command
+    # ethtool
+    # pciutils # lspci
+    # usbutils # lsusb
   ];
 
-  programs.alacritty = {
-    enable = true;
-    # custom settings
-    settings = {
-      env.TERM = "xterm-256color";
-      font = {
-        size = 12;
-        draw_bold_text_with_bright_colors = true;
-      };
-      scrolling.multiplier = 5;
-      selection.save_to_clipboard = true;
-    };
-  };
+  # programs.alacritty = {
+  #   enable = true;
+  #   # custom settings
+  #   settings = {
+  #     env.TERM = "xterm-256color";
+  #     font = {
+  #       size = 12;
+  #       draw_bold_text_with_bright_colors = true;
+  #     };
+  #     scrolling.multiplier = 5;
+  #     selection.save_to_clipboard = true;
+  #   };
+  # };
 
-  programs.bash = {
-    enable = true;
-    enableCompletion = true;
-    # TODO add your custom bashrc here
-    bashrcExtra = ''
-      export PATH="$PATH:$HOME/bin:$HOME/.local/bin:$HOME/go/bin"
-    '';
+  # programs.bash = {
+  #   enable = true;
+  #   enableCompletion = true;
+  #   # TODO add your custom bashrc here
+  #   bashrcExtra = ''
+  #     export PATH="$PATH:$HOME/bin:$HOME/.local/bin:$HOME/go/bin"
+  #   '';
 
-    # set some aliases, feel free to add more or remove some
-    shellAliases = {
-      k = "kubectl";
-      urldecode = "python3 -c 'import sys, urllib.parse as ul; print(ul.unquote_plus(sys.stdin.read()))'";
-      urlencode = "python3 -c 'import sys, urllib.parse as ul; print(ul.quote_plus(sys.stdin.read()))'";
-    };
-  };
+  #   # set some aliases, feel free to add more or remove some
+  #   shellAliases = {
+  #     k = "kubectl";
+  #     urldecode = "python3 -c 'import sys, urllib.parse as ul; print(ul.unquote_plus(sys.stdin.read()))'";
+  #     urlencode = "python3 -c 'import sys, urllib.parse as ul; print(ul.quote_plus(sys.stdin.read()))'";
+  #   };
+  # };
 
-  programs.direnv = {
-    enable = true;
-  };
+  # programs.direnv = {
+  #   enable = true;
+  # };
 
-  programs.firefox = {
-    enable = true;
-  };
+  # programs.firefox = {
+  #   enable = true;
+  # };
 
-  programs.pandoc = {
-    enable = true;
-  };
+  # programs.pandoc = {
+  #   enable = true;
+  # };
 
-  programs.starship = {
-    enable = true;
-    # custom settings
-    settings = {
-      add_newline = false;
-      aws.disabled = true;
-      gcloud.disabled = true;
-      line_break.disabled = true;
-    };
-  };
+  # programs.starship = {
+  #   enable = true;
+  #   # custom settings
+  #   settings = {
+  #     add_newline = false;
+  #     aws.disabled = true;
+  #     gcloud.disabled = true;
+  #     line_break.disabled = true;
+  #   };
+  # };
 }
