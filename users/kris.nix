a6404e92450c06f361f2dcb75129d103b29add8c
Kris Wilk <kris@reefnet.ca>
2025-10-21 10:34:33 -0400

    WIP: refactoring, minimal config for testing

---
+++ users/kris.nix
@@ -0,0 +1,16 @@
+{ config, pkgs, ... }:
+
+{
+  imports = [
+    ./_common.nix
+  ];
+
+  home.username = "kris";
+  home.homeDirectory = "/home/kris";
+
+  programs.git = {
+    enable = true;
+    userName = "Kris Wilk";
+    userEmail = "kris@reefnet.ca";
+  };
+}
