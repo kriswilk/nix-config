bccc4ec386b4555d450cf7feb2739d7211275ddf
Kris Wilk <kris@reefnet.ca>
2025-10-22 01:08:45 -0400

    WIP

--- README.md
+++ README.md
@@ -4,15 +4,8 @@ NixOS system & user configuration based on flakes and home-manager.
 
 ## Usage
 
-1. Boot from a NixOS live CD/USB.
-2. If the host is new, generate `hardware-configuration.nix` and add it to the repo:
+After booting from a NixOS live CD/USB, run:
 
 ```
-sudo nixos-generate-config --no-filesystems --show-hardware-config > hardware-configuration.nix
-```
-
-3. Run the `install.sh` script:
-
-```
-curl https://github.com/kriswilk/nix-config/raw/refs/heads/main/install.sh | bash
+bash <(curl -Ls https://github.com/kriswilk/nix-config/raw/refs/heads/main/install.sh)
 ```
--- install.sh
+++ install.sh
@@ -1,13 +1,29 @@
 #!/usr/bin/env nix-shell
 #!nix-shell -i bash --packages bash disko
 
-read -p "Enter hostname: " hostname
+## COLOURS ##
+C_OFF='\e[0m'
+C_YEL='\e[1;33m'
+C_RED='\e[0;31m'
 
-# prepare disk
-sudo disko --mode destroy,format,mount --flake github:kriswilk/nix-config#${hostname}
+## HELPERS ##
+function notify() { echo -e "\n${C_YEL}${1}${C_OFF}"; sleep 1; }
+function fail() { echo -e "${C_RED}ERROR: ${1}${C_OFF}"; exit 1; }
 
-# install
-sudo nixos-install --no-channel-copy --no-root-password --flake github:kriswilk/nix-config#${hostname}
+notify "HOSTNAME..."
+read -p "Enter the hostname: " host
+[[ -z "${host}" ]] && fail "Hostname cannot be empty"
 
-# clone repo for future use
+notify "GENERATE HARDWARE CONFIG..."
+sudo nixos-generate-config --no-filesystems --show-hardware-config
+echo
+read -s -p "Press Enter to continue..."
+
+notify "RUN DISKO..."
+sudo disko --mode destroy,format,mount --flake github:kriswilk/nix-config#${host}
+
+notify "INSTALL NIXOS..."
+sudo nixos-install --no-channel-copy --no-root-password --flake github:kriswilk/nix-config#${host}
+
+notify "CLONE REPO..."
 sudo git clone https://github.com/kriswilk/nix-config /mnt/etc/nixos
