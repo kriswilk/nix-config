50d32c65f66232953d2824ec8407aade951b89b4
Kris Wilk <kris@reefnet.ca>
2025-10-22 14:43:34 -0400

    WIP

--- disko.nix
+++ disko.nix
@@ -34,17 +34,17 @@
                   "/root" = {
                     mountpoint = "/";
                     mountOptions = [ "compress=zstd" "noatime" ];
-                  };
-                  
+                  };                  
                   "/home" = {
+                    mountpoint = "/home";
                     mountOptions = [ "compress=zstd" "noatime" ];
                   };
-
                   "/nix" = {
+                    mountpoint = "/nix";
                     mountOptions = [ "compress=zstd" "noatime" ];
                   };
-
                   "/swap" = {
+                    mountpoint = "/swap";
                     swap.swapfile.size = "8G";
                   };
                 };                  
