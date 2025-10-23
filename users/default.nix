5f62c9fa272641a4d5b82b80ab83d8fdbf9250a3
Kris Wilk <kris@reefnet.ca>
2025-10-23 16:26:17 -0400

    WIP

--- users/default.nix
+++ users/default.nix
@@ -23,7 +23,6 @@
     direnv.enable = true;
     starship.enable = true;
 
-    htop.enable = true;
     btop.enable = true;
     fastfetch.enable = true;
 
@@ -155,27 +154,17 @@
     # unzip
     # p7zip
 
-    
-
     # # networking tools
     # mtr # A network diagnostic tool
     # iperf3
     # dnsutils  # `dig` + `nslookup`
-    # ldns # replacement of `dig`, it provide the command `drill`
     # aria2 # A lightweight multi-protocol & multi-source command-line download utility
     # socat # replacement of openbsd-netcat
     # nmap # A utility for network discovery and security auditing
-    # ipcalc  # it is a calculator for the IPv4/v6 addresses
 
     # # misc
-    # cowsay
     # file
-    # which
     # tree
-    # gnused
-    # gnutar
-    # gawk
-    # zstd
     # gnupg
 
     # # system call monitoring
@@ -183,11 +172,11 @@
     # ltrace # library call monitoring
     # lsof # list open files
 
-    # # system tools
-    # sysstat
-    # lm_sensors # for `sensors` command
-    # ethtool
-    # pciutils # lspci
-    # usbutils # lsusb
+    # system
+    sysstat # performance
+    lm_sensors # sensors
+    ethtool # ethernet
+    pciutils # pci
+    usbutils # usb
   ];
 }
