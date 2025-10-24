753eba13a2256df76c68ab2714c27c82124daadf
Kris Wilk <kris@reefnet.ca>
2025-10-24 13:43:28 -0400

    WIP

--- home-manager.nix
+++ home-manager.nix
@@ -3,38 +3,14 @@
 let
   systemUsers = config.users.users;
 
- usersDir = "./home";
+  usersDir = "./users";
   
-  mkUserHomeManager = userName: userConfig:
-    let
-      userDir = usersDir + "/${userName}";
-      
-      # Check if the user-specific directory exists and contains a configuration.
-      # This ensures the configuration doesn't break if a user doesn't have a custom file.
-      userFileExists = lib.fileExists (userDir + "/home.nix");
-
-      # List of modules to import for this user.
-      userModules = [
-        # Always import the default shared configuration
-        (usersDir + "/home.nix")
-      ] 
-      # Conditionally add the user-specific configuration if it exists
-      ++ lib.optionals userFileExists (
-        (userDir + "/home.nix")
-      );
-      
-    in
-    # Return the Home Manager module configuration for this user
+  mkUserHomeManager = user: userConfig:
     {
-      # The user's configuration is composed of the dynamic imports
-      imports = userModules;
-      
-      # NOTE: It's good practice to set home.username and home.homeDirectory 
-      # in the user's specific module (e.g., default/home.nix) to ensure 
-      # Home Manager knows who it's configuring, although in a NixOS module
-      # setup, it often auto-detects based on the key ('userName').
-      
-      # You MUST set the stateVersion in one of the imported modules (like default/home.nix).
+      imports = [
+        (usersDir + "/home.nix")
+        (usersDir + "/${user}/home.nix")
+      ];
     };
 
 in
