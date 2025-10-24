dd08cd00b22dc8cdded784a82b6fe45a68f6d40f
Kris Wilk <kris@reefnet.ca>
2025-10-24 13:56:24 -0400

    WIP

--- home-manager.nix
+++ home-manager.nix
@@ -1,26 +1,15 @@
 { config, pkgs, lib, ... }:
 
 let
-  systemUsers = config.users.users;
-
-  usersDir = ./users;
-  
   mkUserHomeManager = user: userConfig:
-    {
-      imports = [
-        (usersDir + "/home.nix")
-        (usersDir + "/${user}/home.nix")
-      ];
-    };
-
+  {
+    imports = [ "./users/home.nix" ".users/${user}/home.nix" ];
+  };
 in
 {
-  # 4. Use mapAttrs to apply the function to every user in the system
-  # The result is an attribute set: { kriswilk = { imports = [...]; }; otheruser = { imports = [...]; }; }
-  # This matches the structure expected by 'home-manager.users'.
   home-manager = {
     useGlobalPkgs = true;
     useUserPackages = true;
-    users = lib.mapAttrs mkUserHomeManager systemUsers;
+    users = lib.mapAttrs mkUserHomeManager config.users.users;
   };
 }
