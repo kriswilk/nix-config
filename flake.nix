c24a5bccde629fd27261c8b8d5ab836e1e625336
Kris Wilk <kris@reefnet.ca>
2025-10-25 14:17:19 -0400

    WIP

--- flake.nix
+++ flake.nix
@@ -29,8 +29,8 @@
 
     # hosts
     hostDir = ./host;
-    hosts = lib.filterAttrs (name: type: type == "directory") (builtins.readDir hostDir);
-    mkNixosSystem = host: type:
+    hosts = builtins.attrNames (lib.filterAttrs (name: type: type == "directory") (builtins.readDir hostDir));
+    mkNixosSystem = host:
       lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
