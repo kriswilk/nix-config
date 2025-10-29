cf12862acceb98c629b40caf0e6476d5eb88fc49
Kris Wilk <kris@reefnet.ca>
2025-10-29 15:10:23 -0400

    WIP

--- home/default.nix
+++ home/default.nix
@@ -1,29 +1,23 @@
 { config, lib, pkgs, ... }:
 
-{
-  imports = [
-    ./_module/archive.nix
-    ./_module/audio.nix
-    ./_module/browser.nix
-    ./_module/cad.nix
-    ./_module/development.nix
-    ./_module/disk.nix
-    ./_module/editor.nix
-    ./_module/file-management.nix
-    ./_module/font.nix
-    ./_module/game.nix
-    ./_module/image.nix
-    ./_module/network.nix
-    ./_module/nixos.nix
-    ./_module/office.nix
-    ./_module/print-scan.nix
-    ./_module/security.nix
-    ./_module/shell.nix
-    ./_module/system.nix
-    ./_module/terminal.nix
-    ./_module/text.nix
-    ./_module/video.nix
-  ];
+let
+  moduleDir = ./_module;
+
+  dirContents = builtins.readDir modulesPath;
+
+  nixFiles = lib.attrNames (
+    lib.filterAttrs
+      (name: type: type == "regular" && lib.strings.hasSuffix ".nix" name)
+      dirContents
+  );
+
+  modulePaths = builtins.map
+    (name: moduleDir + "/${name}")
+    nixFiles;
+
+in {
+  # import all modules
+  imports = modulePaths;
 
   home.stateVersion = "25.05";
   programs.home-manager.enable = true;
