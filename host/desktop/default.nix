48af43511d5fed5faf094ee97e782726837da781
Kris Wilk <kris@reefnet.ca>
2025-10-31 12:26:24 -0400

    WIP

--- host/desktop/default.nix
+++ host/desktop/default.nix
@@ -4,7 +4,7 @@
   imports = [
     ./hardware-configuration.nix
     ../.
-    ../_module/custom/gpu_nvidia_open.nix
+    ../_module/_gpu_nvidia_open.nix
   ];
 
   disko.devices.disk.main.device = "/dev/nvme0n1";
