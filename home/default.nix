664b63367fadf5e66073dc2d531e29d36c73c364
Kris Wilk <kris@reefnet.ca>
2025-10-31 12:15:10 -0400

    WIP

--- home/default.nix
+++ home/default.nix
@@ -1,7 +1,9 @@
-{ config, lib, pkgs, listNixFilesRecursive, ... }:
+{ config, lib, pkgs, ... }:
 
 {
-  imports = listNixFilesRecursive ./_module;
+  imports = lib.fileset.toList (
+    lib.fileset.fileFilter (file: file.hasExt "nix" && ! lib.hasPrefix "_" file.name) ./_module
+  );
 
   home.stateVersion = "25.05";
   programs.home-manager.enable = true;
