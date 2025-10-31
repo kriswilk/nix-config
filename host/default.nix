e3bc43e21edcd77be5f0300c615ca6215d05131b
Kris Wilk <kris@reefnet.ca>
2025-10-31 11:30:23 -0400

    WIP

--- host/default.nix
+++ host/default.nix
@@ -2,7 +2,7 @@
 
 {
   imports = lib.fileset.toList (
-    lib.fileset.fileFilter (file: file.hasExt "nix" && ! builtins.hasPrefix "_" file.name) ../module/host
+    lib.fileset.fileFilter (file: file.hasExt "nix" && ! lib.hasPrefix "_" file.name) ../module/host
   );
 
   system.stateVersion = "25.05";
