53609ba74874e43b7558b94541ecac7c26479aac
Kris Wilk <kris@reefnet.ca>
2025-10-31 11:37:02 -0400

    WIP

--- home/default.nix
+++ home/default.nix
@@ -2,7 +2,7 @@
 
 {
   imports = lib.fileset.toList (
-    lib.fileset.fileFilter (file: file.hasExt "nix" && ! lib.hasPrefix "_" file.name) ../module/home
+    lib.fileset.fileFilter (file: file.hasExt "nix" && ! lib.hasPrefix "_" file.name) ./_module
   );
 
   home.stateVersion = "25.05";
--- host/default.nix
+++ host/default.nix
@@ -2,7 +2,7 @@
 
 {
   imports = lib.fileset.toList (
-    lib.fileset.fileFilter (file: file.hasExt "nix" && ! lib.hasPrefix "_" file.name) ../module/host
+    lib.fileset.fileFilter (file: file.hasExt "nix" && ! lib.hasPrefix "_" file.name) ./_module
   );
 
   system.stateVersion = "25.05";
