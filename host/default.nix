b9478abda2cbbc590f2dd63c77e7c3291c171e1a
Kris Wilk <kris@reefnet.ca>
2025-10-31 12:24:01 -0400

    WIP

--- host/default.nix
+++ host/default.nix
@@ -1,7 +1,7 @@
 { config, lib, pkgs, listNixFilesRecursive, ... }:
 
 {
-  imports = dir: lib.fileset.toList (
+  imports = lib.fileset.toList (
       lib.fileset.fileFilter (file: file.hasExt "nix" && ! lib.hasPrefix "_" file.name) ./_module
     );
 
