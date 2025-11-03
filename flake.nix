1a70213ff5ebdc1dd8368fa1654998ed41212bbe
Kris Wilk <kris@reefnet.ca>
2025-11-03 11:25:24 -0500

    WIP

--- flake.nix
+++ flake.nix
@@ -17,29 +17,29 @@
 
   outputs = { self, nixpkgs, disko, home-manager, ... }:
   let
-    nixosConfigs = {
-      vm = {};
-      desktop = {};
-    };
+    configNames = [ "vm" "desktop" ];
   in {
-    nixosConfigurations = nixpkgs.lib.mapAttrs (configName: configData:
-      nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-          ./host/${configName}.nix
-          disko.nixosModules.disko
-          home-manager.nixosModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.users.kris = ./home/kris.nix;
-            home-manager.users.guest = ./home/guest.nix;
-          }
-        ];
-        specialArgs = {
-          inherit configName;
+    nixosConfigurations = builtins.listToAttrs (builtins.map (configName:
+      {
+        name = configName;
+        value = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          modules = [
+            ./host/${configName}.nix
+            disko.nixosModules.disko
+            home-manager.nixosModules.home-manager
+            {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.users.kris = ./home/kris.nix;
+              home-manager.users.guest = ./home/guest.nix;
+            }
+          ];
+          specialArgs = {
+            inherit configName;
+          };
         };
       }
-    ) nixosConfigs;
+    ) configNames);
   };
 }
