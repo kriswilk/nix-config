cf811899982276a8769a9873d1dd799416d906d6
Kris Wilk <kris@reefnet.ca>
2025-11-03 12:34:21 -0500

    WIP

--- host/modules/users.nix
+++ host/modules/users.nix
@@ -18,7 +18,7 @@
     useGlobalPkgs = true;
     useUserPackages = true;
     users = lib.mapAttrs (userName: userData:
-      "./user/${userName}.nix"
+      "./users/${userName}.nix"
     ) userList;
   };
 }
