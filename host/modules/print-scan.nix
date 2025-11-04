9590fe163a45019cbbf7cad68e8375dd01cf8242
Kris Wilk <kris@reefnet.ca>
2025-11-04 11:37:15 -0500

    WIP

--- host/modules/print-scan.nix
+++ host/modules/print-scan.nix
@@ -10,13 +10,13 @@
     ensureDefaultPrinter = "printer-office";
     ensurePrinters = [
       {
-        deviceUri = "ipp://10.0.0.215";
+        deviceUri = "ipp://10.0.0.215/ipp";
         name = "printer-office";
         description = "Canon MF455dw";
         model = "everywhere";
       }
       {
-        deviceUri = "ipp://10.0.0.216";
+        deviceUri = "ipp://10.0.0.216/ipp";
         name = "printer-kris";
         description = "Canon MF455dw";
         model = "everywhere";
