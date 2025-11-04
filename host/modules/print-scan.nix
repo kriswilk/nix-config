d6f3156bd791ae2dd0fc163e18208e95a65393ce
Kris Wilk <kris@reefnet.ca>
2025-11-04 12:11:25 -0500

    WIP

--- host/modules/print-scan.nix
+++ host/modules/print-scan.nix
@@ -15,29 +15,29 @@
     ];
   };
 
-  hardware.printers = {
-    ensureDefaultPrinter = "printer-office";
-    ensurePrinters = [
-      {
-        deviceUri = "ipp://10.0.0.215/ipp";
-        name = "printer-office";
-        description = "Canon MF455dw";
-        model = "everywhere";
-      }
-      {
-        deviceUri = "ipp://10.0.0.216/ipp";
-        name = "printer-kris";
-        description = "Canon MF455dw";
-        model = "everywhere";
-      }
-      {
-        deviceUri = "ipp://10.0.0.203:631/ipp";
-        name = "printer-daniela";
-        description = "Brother DCP-7065DN";
-        model = "everywhere";
-      }
-    ];
-  };
+  # hardware.printers = {
+  #   ensureDefaultPrinter = "printer-office";
+  #   ensurePrinters = [
+  #     {
+  #       deviceUri = "ipp://10.0.0.215/ipp";
+  #       name = "printer-office";
+  #       description = "Canon MF455dw";
+  #       model = "everywhere";
+  #     }
+  #     {
+  #       deviceUri = "ipp://10.0.0.216/ipp";
+  #       name = "printer-kris";
+  #       description = "Canon MF455dw";
+  #       model = "everywhere";
+  #     }
+  #     {
+  #       deviceUri = "ipp://10.0.0.203:631/ipp";
+  #       name = "printer-daniela";
+  #       description = "Brother DCP-7065DN";
+  #       model = "everywhere";
+  #     }
+  #   ];
+  # };
 
   hardware.sane = {
     enable = true;
