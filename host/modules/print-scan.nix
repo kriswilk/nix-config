2131fc99b30ae64084da8e8b753713a51071a364
Kris Wilk <kris@reefnet.ca>
2025-11-04 14:12:01 -0500

    WIP

--- host/modules/print-scan.nix
+++ host/modules/print-scan.nix
@@ -8,23 +8,23 @@
 
   services.printing = {
     enable = true;
-    # drivers = with pkgs; [
-    #   cups-filters
-    #   cups-browsed
-    # #   # common driver packages as per wiki.nixos.org
-    # #   gutenprint # Drivers for many different printers from many different vendors.
-    # #   gutenprintBin # Additional, binary-only drivers for some printers.
-    # #   hplip # Drivers for HP printers.
-    # #   postscript-lexmark # Postscript drivers for Lexmark
-    # #   samsung-unified-linux-driver # Proprietary Samsung Drivers
-    # #   splix # Drivers for printers supporting SPL (Samsung Printer Language).
-    # #   brlaser # Drivers for some Brother printers
-    # #   brgenml1lpr # Generic drivers for more Brother printers (Proprietary drivers)
-    # #   brgenml1cupswrapper # Generic drivers for more Brother printers (Proprietary drivers)
-    # #   cnijfilter2 # Drivers for some Canon Pixma devices (Proprietary driver)
-    # #   epson-escpr2 # Drivers for newer Epson devices.
-    # #   epson-escpr # Drivers for some other Epson devices.
-    # ];
+    drivers = with pkgs; [
+      cups-filters
+      cups-browsed
+    #   # common driver packages as per wiki.nixos.org
+    #   gutenprint # Drivers for many different printers from many different vendors.
+    #   gutenprintBin # Additional, binary-only drivers for some printers.
+    #   hplip # Drivers for HP printers.
+    #   postscript-lexmark # Postscript drivers for Lexmark
+    #   samsung-unified-linux-driver # Proprietary Samsung Drivers
+    #   splix # Drivers for printers supporting SPL (Samsung Printer Language).
+    #   brlaser # Drivers for some Brother printers
+    #   brgenml1lpr # Generic drivers for more Brother printers (Proprietary drivers)
+    #   brgenml1cupswrapper # Generic drivers for more Brother printers (Proprietary drivers)
+    #   cnijfilter2 # Drivers for some Canon Pixma devices (Proprietary driver)
+    #   epson-escpr2 # Drivers for newer Epson devices.
+    #   epson-escpr # Drivers for some other Epson devices.
+    ];
   };
 
   # hardware.printers = {
