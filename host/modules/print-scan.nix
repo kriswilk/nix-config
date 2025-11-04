d51ab13de3a42dcacd1f3e667037459a90076f1c
Kris Wilk <kris@reefnet.ca>
2025-11-04 15:35:47 -0500

    WIP

--- host/modules/print-scan.nix
+++ host/modules/print-scan.nix
@@ -31,20 +31,20 @@
     { name        = "printer-office";
       description = "Canon MF455dw";
       location    = "fathoms";
-      deviceUri   = "socket://10.0.0.215:9100";
-      model       = "gutenprint.5.3://pcl-g_5/expert";
+      deviceUri   = "socket://10.0.0.215";
+      model       = "gutenprint.5.3://pcl-g_6/expert";
     }
     { name        = "printer-kris";
       description = "Canon MF455dw";
       location    = "fathoms";
-      deviceUri   = "socket://10.0.0.216:9100";
-      model       = "gutenprint.5.3://pcl-g_5/expert";
+      deviceUri   = "socket://10.0.0.216";
+      model       = "gutenprint.5.3://pcl-g_6/expert";
     }
     { name        = "printer-daniela";
       description = "Brother DCP-7065DN";
       location    = "fathoms";
-      deviceUri   = "socket://10.0.0.203:9100";
-      model       = "gutenprint.5.3://pcl-g_5/expert";
+      deviceUri   = "socket://10.0.0.203";
+      model       = "gutenprint.5.3://pcl-g_6/expert";
     }
   ];
 
