cbf5fc17a4efe95b978b5a2f950b15d40cb69d18
Kris Wilk <kris@reefnet.ca>
2025-11-04 11:09:11 -0500

    WIP

--- host/modules/print-scan.nix
+++ host/modules/print-scan.nix
@@ -6,6 +6,18 @@
     # WIP needs testing/config
   };
 
+  hardware.printers = {
+    ensureDefaultPrinter = "Canon Laser";
+    ensurePrinters = [
+      {
+        deviceUri = "ipp://10.0.0.216/ipp";
+        location = "home";
+        name = "Canon Laser";
+        model = "everywhere";
+      }
+    ];
+  };
+
   hardware.sane = {
     enable = true;
   };
