b2b5c46a95bdc1a8b669967f888c9517bc24eef7
Kris Wilk <kris@reefnet.ca>
2025-11-04 14:32:51 -0500

    WIP

--- host/modules/print-scan.nix
+++ host/modules/print-scan.nix
@@ -1,31 +1,48 @@
 { config, lib, pkgs, ... }:
 
 {
+services.printing.enable = true;
+
+services.printing.drivers = lib.singleton (pkgs.linkFarm "drivers" [
+  { name = "share/cups/model/printer-kris.ppd";
+    path = ./printer-kris.ppd;
+  }
+]);
+
+hardware.printers.ensurePrinters = [
+  { name        = "printer-kris";
+    description = "Canon Printer";
+    location    = "fathomstown";
+    deviceUri   = "ipp://printer-kris.local/ipp";
+    model       = "printer-kris.ppd";
+  }
+];
+
   # services.avahi = {
   #   enable = true;
   #   nssmdns4 = true;
   # };
 
-  services.printing = {
-    enable = true;
-    drivers = with pkgs; [
-      cups-filters
-      cups-browsed
-    #   # common driver packages as per wiki.nixos.org
-    #   gutenprint # Drivers for many different printers from many different vendors.
-    #   gutenprintBin # Additional, binary-only drivers for some printers.
-    #   hplip # Drivers for HP printers.
-    #   postscript-lexmark # Postscript drivers for Lexmark
-    #   samsung-unified-linux-driver # Proprietary Samsung Drivers
-    #   splix # Drivers for printers supporting SPL (Samsung Printer Language).
-    #   brlaser # Drivers for some Brother printers
-    #   brgenml1lpr # Generic drivers for more Brother printers (Proprietary drivers)
-    #   brgenml1cupswrapper # Generic drivers for more Brother printers (Proprietary drivers)
-    #   cnijfilter2 # Drivers for some Canon Pixma devices (Proprietary driver)
-    #   epson-escpr2 # Drivers for newer Epson devices.
-    #   epson-escpr # Drivers for some other Epson devices.
-    ];
-  };
+  # services.printing = {
+  #   enable = true;
+  #   drivers = with pkgs; [
+  #     cups-filters
+  #     cups-browsed
+  #   #   # common driver packages as per wiki.nixos.org
+  #   #   gutenprint # Drivers for many different printers from many different vendors.
+  #   #   gutenprintBin # Additional, binary-only drivers for some printers.
+  #   #   hplip # Drivers for HP printers.
+  #   #   postscript-lexmark # Postscript drivers for Lexmark
+  #   #   samsung-unified-linux-driver # Proprietary Samsung Drivers
+  #   #   splix # Drivers for printers supporting SPL (Samsung Printer Language).
+  #   #   brlaser # Drivers for some Brother printers
+  #   #   brgenml1lpr # Generic drivers for more Brother printers (Proprietary drivers)
+  #   #   brgenml1cupswrapper # Generic drivers for more Brother printers (Proprietary drivers)
+  #   #   cnijfilter2 # Drivers for some Canon Pixma devices (Proprietary driver)
+  #   #   epson-escpr2 # Drivers for newer Epson devices.
+  #   #   epson-escpr # Drivers for some other Epson devices.
+  #   ];
+  # };
 
   # hardware.printers = {
   #   ensureDefaultPrinter = "printer-office";
