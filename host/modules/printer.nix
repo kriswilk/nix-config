547b0b454880f83449c59cb79b5b8015b520dbd9
Kris Wilk <kris@reefnet.ca>
2025-11-05 11:35:36 -0500

    WIP

--- host/modules/printer.nix
+++ host/modules/printer.nix
@@ -15,33 +15,79 @@
     ];
   };
 
+  # WIP: need better naming scheme for printers
   hardware.printers.ensurePrinters = [
-    { name        = "fathoms-office";
-      description = "Canon MF455dw";
-      location    = "fathom";
-      deviceUri   = "socket://10.0.0.215";
-      model       = "drv:///sample.drv/generic.ppd";
-    }
-    { name        = "fathoms-kris";
-      description = "Canon MF455dw";
-      location    = "fathom";
-      deviceUri   = "socket://10.0.0.216";
-      model       = "drv:///sample.drv/generic.ppd";
-    }
-    { name        = "fathoms-daniela";
+    {
+      name = "fathoms-daniela";
       description = "Brother DCP-7065DN";
-      location    = "fathom";
-      deviceUri   = "socket://10.0.0.203";
-      model       = "brother-BrGenML1-cups-en.ppd";
+      location = "fathoms";
+      deviceUri = "socket://10.0.0.203";
+      model = "brother-BrGenML1-cups-en.ppd";
+      ppdOptions = {
+        PageSize = "Letter";
+      };
     }
-    { name        = "fathoms-thermal";
+    {
+      name = "fathoms-kris";
+      description = "Canon MF455dw";
+      location = "fathoms";
+      deviceUri = "socket://10.0.0.216";
+      model = "drv:///sample.drv/generpcl.ppd";
+      ppdOptions = {
+        PageSize = "Letter";
+        Option1 = "True";
+      };
+    }
+    {
+      name = "fathoms-office";
+      description = "Canon MF455dw";
+      location = "fathoms";
+      deviceUri = "socket://10.0.0.215";
+      model = "drv:///sample.drv/generpcl.ppd";
+      ppdOptions = {
+        PageSize = "Letter";
+        Option1 = "True";
+      };
+    }
+    {
+      name = "fathoms-thermal";
       description = "Zebra ZT230";
-      location    = "fathom";
-      deviceUri   = "socket://10.0.0.214";
-      model       = "drv:///sample.drv/zebra.ppd";
-      ppdOptions  = {
-                      PageSize = "w288h432"; # 4in x 6in
-                    };
+      location = "fathoms";
+      deviceUri = "socket://10.0.0.214";
+      model = "drv:///sample.drv/zebra.ppd";
+      ppdOptions = {
+        PageSize = "w288h432"; # 4in x 6in
+      };
     }
+    # {
+    #   name        = "printer-reefnet-office";
+    #   description = "Brother MFC-7460DN";
+    #   location    = "reefnet";
+    #   deviceUri   = "socket://192.168.1.201";
+    #   model       = "brother-BrGenML1-cups-en.ppd";
+    #   ppdOptions  = {
+    #     PageSize = "Letter";
+    #   };
+    # }
+    # {
+    #   name        = "printer-reefnet-colour";
+    #   description = "Lexmark C3224dw";
+    #   location    = "reefnet";
+    #   deviceUri   = "socket://192.168.1.202";
+    #   model       = "drv:///sample.drv/generic.ppd";
+    #   ppdOptions  = {
+    #     PageSize = "Letter";
+    #   };
+    # }
+    # {
+    #   name        = "printer-reefnet-thermal";
+    #   description = "Zebra ZT230";
+    #   location    = "reefnet";
+    #   deviceUri   = "socket://192.168.1.212";
+    #   model       = "drv:///sample.drv/zebra.ppd";
+    #   ppdOptions  = {
+    #     PageSize = "w288h432"; # 4in x 6in
+    #   };
+    # }
   ];
 }
