0daa52195bc5b26cc4238102df548127495d7a34
Kris Wilk <kris@reefnet.ca>
2025-11-05 09:55:03 -0500

    WIP

--- host/modules/printer.nix
+++ host/modules/printer.nix
@@ -8,7 +8,7 @@
 
   services.printing = {
     enable = true;
-    #browsed.enable = false; # don't auto-configure printers
+    browsed.enable = false; # don't auto-configure printers
     drivers = with pkgs; [
       cups-filters
       cups-browsed
