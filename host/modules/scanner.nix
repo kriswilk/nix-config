a2acce0c27e987d9ab04344ef0b84ec20c74d9fc
Kris Wilk <kris@reefnet.ca>
2025-11-05 09:40:34 -0500

    WIP

--- host/modules/scanner.nix
+++ host/modules/scanner.nix
@@ -1,14 +1,17 @@
 { config, lib, pkgs, ... }:
 
 {
+  # WIP: needs testing/config
   hardware.sane = {
     enable = true;
     brscan4 = {
-        enable = true;
-        netDevices = {
-          home = { model = "DCP-7065DN"; ip = "10.0.0.203"; };
+      enable = true;
+      netDevices = {
+        "fathoms-daniela" = {
+          ip = "10.0.0.203";
+          model = "DCP-7065DN";
         };
       };
+    };
   };
-  # WIP needs testing/config
 }
