f502e65fb27bb3e28a8d6d7c7a694781a98a6559
Kris Wilk <kris@reefnet.ca>
2025-11-05 09:00:48 -0500

    WIP

--- host/modules/print-scan.nix
+++ host/modules/print-scan.nix
@@ -10,47 +10,29 @@
     enable = true;
     browsed.enable = false;
     drivers = with pkgs; [
-      # common driver packages as per wiki.nixos.org
-      #gutenprint # Drivers for many different printers from many different vendors.
-      #gutenprintBin # Additional, binary-only drivers for some printers.
-      #hplip # Drivers for HP printers.
-      #postscript-lexmark # Postscript drivers for Lexmark
-      #samsung-unified-linux-driver # Proprietary Samsung Drivers
-      #splix # Drivers for printers supporting SPL (Samsung Printer Language).
-      #brlaser # Drivers for some Brother printers
-      brgenml1lpr # Generic drivers for more Brother printers (Proprietary drivers)
-      brgenml1cupswrapper # Generic drivers for more Brother printers (Proprietary drivers)
-      #cnijfilter2 # Drivers for some Canon Pixma devices (Proprietary driver)
-      #epson-escpr2 # Drivers for newer Epson devices.
-      #epson-escpr # Drivers for some other Epson devices.
+      brgenml1lpr
+      brgenml1cupswrapper
     ];
   };
 
   hardware.printers.ensurePrinters = [
     { name        = "fathoms-office";
       description = "Canon MF455dw";
-      location    = "fathoms";
       deviceUri   = "socket://10.0.0.215";
       model       = "drv:///sample.drv/generic.ppd";
     }
-    { name        = "printer-kris";
+    { name        = "fathoms-kris";
       description = "Canon MF455dw";
-      location    = "fathoms";
       deviceUri   = "socket://10.0.0.216";
       model       = "drv:///sample.drv/generic.ppd";
     }
-    { name        = "printer-daniela";
+    { name        = "fathoms-daniela";
       description = "Brother DCP-7065DN";
-      location    = "fathoms";
-      deviceUri   = "socket://10.0.0.203";
+      deviceUri   = "10.0.0.203";
       model       = "brother-BrGenML1-cups-en.ppd";
-      ppdOptions  = {
-                      PageSize = "Letter";
-                    };
     }
-    { name        = "printer-thermal";
+    { name        = "fathoms-thermal";
       description = "Zebra ZT230";
-      location    = "fathoms";
       deviceUri   = "socket://10.0.0.214";
       model       = "drv:///sample.drv/zebra.ppd";
       ppdOptions  = {
