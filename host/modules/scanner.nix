3535cf1d0d72720ccd55456591e7938097c3af4a
Kris Wilk <kris@reefnet.ca>
2025-11-05 22:14:06 -0500

    WIP

--- host/modules/scanner.nix
+++ host/modules/scanner.nix
@@ -6,12 +6,12 @@
     enable = true;
     brscan4 = {
       enable = true;
-      netDevices = {
-        "fathoms-daniela" = {
-          ip = "10.0.0.203";
-          model = "DCP-7065DN";
-        };
-      };
+#      netDevices = {
+#        "fathoms-daniela" = {
+#          ip = "10.0.0.203";
+#          model = "DCP-7065DN";
+#        };
+#      };
     };
   };
 }
