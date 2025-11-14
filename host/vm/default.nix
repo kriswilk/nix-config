c7ebd0b51a52d83e9b1da3dd97b69f72505d890d
Kris Wilk <kris@reefnet.ca>
2025-11-14 10:17:08 -0500

    WIP

--- host/vm/default.nix
+++ host/vm/default.nix
@@ -3,17 +3,8 @@
 {
   imports = [
     ../_modules
+    ./hardware-configuration.nix
   ];
 
   disko.devices.disk.main.device = "/dev/sda";
-
-  ### hardware-configuration.nix ###
-  
-  boot.initrd.availableKernelModules = [ "ata_piix" "ohci_pci" "ehci_pci" "ahci" "sd_mod" "sr_mod" ];
-  boot.initrd.kernelModules = [ ];
-  boot.kernelModules = [ ];
-  boot.extraModulePackages = [ ];
-
-  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
-  virtualisation.virtualbox.guest.enable = true;
 }
