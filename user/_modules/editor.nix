78832f0725c1f0e3e6a5a20487edfd33b8d65192
Kris Wilk <kris@reefnet.ca>
2025-12-01 13:45:26 -0500

    WIP

--- user/_modules/editor.nix
+++ user/_modules/editor.nix
@@ -3,23 +3,12 @@
 {
   programs.nvf = {
     enable = true;
-    # your settings need to go into the settings attribute set
-    # most settings are documented in the appendix
     settings = {
-      vim.viAlias = false;
-      vim.vimAlias = true;
       vim.lsp = {
         enable = true;
       };
     };
   };
-  
-  # programs.neovim = {
-  #   enable = true;
-  #   viAlias = true;
-  #   vimAlias = true;
-  #   vimdiffAlias = true;
-  # };
 
   home.packages = with pkgs; [
     #imhex
