9bd1d05fc8ef46f07ff1b6cb071cdced30b2fde4
Kris Wilk <kris@reefnet.ca>
2025-12-01 13:27:56 -0500

    WIP

--- flake.nix
+++ flake.nix
@@ -63,13 +63,12 @@
           "${hostDir}/${hostName}"
           disko.nixosModules.disko
           home-manager.nixosModules.home-manager
-          { home-manager.extraSpecialArgs = { inherit inputs; }; }
           stylix.nixosModules.stylix
-          # {
-          #   home-manager.sharedModules = [
-          #     nvf.homeManagerModules.default
-          #   ];
-          # }
+          {
+            home-manager.modules = [
+              nvf.homeManagerModules.default
+            ];
+          }
         ];
         specialArgs = { inherit hostDir hostName userDir userList; };
       }
