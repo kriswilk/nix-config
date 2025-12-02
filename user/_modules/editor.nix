3f30a510f6965ba6b6fe7e7dc1222b11ce40c88b
Kris Wilk <kris@reefnet.ca>
2025-12-02 00:10:37 -0500

    WIP

--- user/_modules/editor.nix
+++ user/_modules/editor.nix
@@ -6,9 +6,9 @@
     settings.vim = {
       viAlias = false;
       vimAlias = false;
-      lsp = {
-        enable = true;
-      };
+      
+      #lsp.enable = true;
+
       statusline.lualine.enable = true;
       telescope.enable = true;
       autocomplete.nvim-cmp.enable = true;
