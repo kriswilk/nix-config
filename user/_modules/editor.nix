7281908c8e3d6e5f09ba5ddbf4232de3a930a34e
Kris Wilk <kris@reefnet.ca>
2025-12-02 00:00:50 -0500

    WIP

--- user/_modules/editor.nix
+++ user/_modules/editor.nix
@@ -3,11 +3,23 @@
 {
   programs.nvf = {
     enable = true;
-    settings = {
-      vim.viAlias = false;
-      vim.vimAlias = false;
-      vim.lsp = {
+    settings.vim = {
+      viAlias = false;
+      vimAlias = false;
+      lsp = {
         enable = true;
+      };
+      statusline.lualine.enable = true;
+      telescope.enable = true;
+      autocomplete.nvim-cmp.enable = true;
+      languages = {
+        enableTreeSitter = true;
+        
+        nix.enable = true;
+        ts.enable = true;
+        python.enable = true;
+        lua.enable = true;
+        rust.enable = true;
       };
     };
   };
