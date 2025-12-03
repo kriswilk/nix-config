6d15ffd05b57271dafb5d4d00d946eed5a27bbb7
Kris Wilk <kris@reefnet.ca>
2025-12-03 14:04:50 -0500

    WIP

--- user/_modules/stylix.nix
+++ user/_modules/stylix.nix
@@ -4,8 +4,8 @@
   stylix = {
     enable = true;
     #base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";
-    base16Scheme = "${pkgs.base16-schemes}/share/themes/dark-plus.yaml";
-    #base16Scheme = "${pkgs.base16-schemes}/share/themes/tokyodark.yaml";
+    #base16Scheme = "${pkgs.base16-schemes}/share/themes/dark-plus.yaml";
+    base16Scheme = "${pkgs.base16-schemes}/share/themes/tokyodark.yaml";
     #base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-hard.yaml";
     
     # override = {
