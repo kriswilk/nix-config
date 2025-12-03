970ba8089fffc532b69b712765f04a8fe4e0299a
Kris Wilk <kris@reefnet.ca>
2025-12-03 12:43:53 -0500

    WIP

--- host/_modules/stylix.nix
+++ host/_modules/stylix.nix
@@ -4,10 +4,10 @@
   stylix = {
     enable = true;
     base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-hard.yaml";
-    override = {
-      # pure black background
-      scheme = "gruvbox-dark-hard-black";
-      base00 = "000000";
-    };
+    # override = {
+    #   # pure black background
+    #   scheme = "gruvbox-dark-hard-black";
+    #   base00 = "000000";
+    # };
   };
 }
