ade4b2be6f1c92c163d14c93b305e26e878bc52d
Kris Wilk <kris@reefnet.ca>
2025-12-03 15:42:09 -0500

    WIP

--- host/_modules/stylix.nix
+++ host/_modules/stylix.nix
@@ -3,15 +3,11 @@
 {
   stylix = {
     enable = true;
-    #base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";
-    #base16Scheme = "${pkgs.base16-schemes}/share/themes/dark-plus.yaml";
-    base16Scheme = "${pkgs.base16-schemes}/share/themes/tokyo-night-dark.yaml";
-    #base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-hard.yaml";
+    base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";
     
-    # override = {
-    #   # pure black background
-    #   scheme = "gruvbox-dark-hard-black";
-    #   base00 = "000000";
-    # };
+    override = {
+      scheme = "Catppuccin Mocha OLED";
+      base00 = "000000";
+    };
   };
 }
