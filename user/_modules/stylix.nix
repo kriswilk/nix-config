9efa19666d387f0bd9f66dcd972a8958801bcfcb
Kris Wilk <kris@reefnet.ca>
2025-12-03 11:32:11 -0500

    WIP

--- user/_modules/stylix.nix
+++ user/_modules/stylix.nix
@@ -4,10 +4,22 @@
   stylix = {
     enable = true;
     base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-hard.yaml";
+    
     override = {
       # pure black background
       scheme = "gruvbox-dark-hard-black";
       base00 = "000000";
+    };
+
+    fonts = {
+      monospace = {
+        package = pkgs.nerd-fonts.jetbrains-mono;
+        name = "JetBrains Mono Nerd Font";
+      };
+
+      serif = monospace;
+      sansSerif = monospace;
+      emoji = monospace;
     };
   };
 }
