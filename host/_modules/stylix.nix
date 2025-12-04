7a18054f9c4360e4ccab4b8bae621589e3330532
Kris Wilk <kris@reefnet.ca>
2025-12-04 13:13:01 -0500

    WIP

--- host/_modules/stylix.nix
+++ host/_modules/stylix.nix
@@ -4,7 +4,7 @@
   stylix = {
     enable = true;
     base16Scheme = pkgs.fetchurl {
-      url = "https://github.com/kriswilk/themes/raw/refs/heads/main/ekko/base16/ekko-dark.yaml";
+      url = "https://github.com/kriswilk/themes/raw/refs/heads/main/base16/ekko-dark.yaml";
       sha256 = lib.fakeSha256;
     };
   };
