ec1bb5ff3bdae0b62da2ff5124d7e8e0d6844ad8
Kris Wilk <kris@reefnet.ca>
2025-12-03 23:33:17 -0500

    WIP

--- host/_modules/stylix.nix
+++ host/_modules/stylix.nix
@@ -6,7 +6,22 @@
     base16Scheme = "${pkgs.base16-schemes}/share/themes/tokyodark.yaml";
     
     override = {
-      base00 = "000000";
+      base00= "#000000"; # black
+      base01= "#242424"; # gray01
+      base02= "#494949"; # gray02
+      base03= "#6d6d6d"; # gray03
+      base04= "#929292"; # gray04
+      base05= "#b6b6b6"; # gray05
+      base06= "#dbdbdb"; # gray06
+      base07= "#ffffff"; # white
+      base08= "#fb0120"; # red
+      base09= "#fc6d24"; # orange
+      base0A= "#fda331"; # yellow
+      base0B= "#a1c659"; # green
+      base0C= "#76c7b7"; # cyan
+      base0D= "#6fb3d2"; # blue
+      base0E= "#d381c3"; # purple
+      base0F= "#be643c"; # brown
     };
   };
 }
