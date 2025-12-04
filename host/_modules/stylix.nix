5f06e074a6d378d9e42e097b69309b980622cf81
Kris Wilk <kris@reefnet.ca>
2025-12-03 22:21:09 -0500

    WIP

--- host/_modules/stylix.nix
+++ host/_modules/stylix.nix
@@ -3,12 +3,10 @@
 {
   stylix = {
     enable = true;
-    #base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";
     base16Scheme = "${pkgs.base16-schemes}/share/themes/tokyodark.yaml";
     
-    # override = {
-    #   scheme = "Catppuccin Mocha OLED";
-    #   base00 = "000000";
-    # };
+    override = {
+      base00 = "000000";
+    };
   };
 }
