5894b5e9c87f7ea5c4275689db3eab65e7b1a072
Kris Wilk <kris@reefnet.ca>
2025-12-04 23:58:46 -0500

    WIP

--- user/_modules/stylix.nix
+++ user/_modules/stylix.nix
@@ -11,7 +11,7 @@
 
     fonts = {
       monospace = {
-        package = pkgs.nerdfonts.override {fonts = ["FiraCode"];};
+        package = pkgs.nerd-fonts.fira-code;
       name = "FiraCode Nerd Font";
       };
     };
