1b6c0ca3ea8cb805e945a6c2ac94b7bcccf9bebb
Kris Wilk <kris@reefnet.ca>
2025-12-05 10:36:33 -0500

    WIP

--- user/_modules/shell.nix
+++ user/_modules/shell.nix
@@ -13,7 +13,7 @@
     enable = true;
     settings = lib.mkMerge [
       (builtins.fromTOML
-        (builtins.readFile "${pkgs.starship}/share/starship/presets/plain-text-symbols.toml"
+        (builtins.readFile "${pkgs.starship}/share/starship/presets/nerd-font-symbols.toml"
       ))
       {
         # WIP: other custom configuration here
