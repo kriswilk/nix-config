f22c11bad60b3d01431187dd40c5363d7bad6f28
Kris Wilk <kris@reefnet.ca>
2025-12-04 22:49:26 -0500

    WIP

--- user/_modules/stylix.nix
+++ user/_modules/stylix.nix
@@ -3,9 +3,17 @@
 {
   stylix = {
     enable = true;
+
     base16Scheme = builtins.fetchurl {
       url = "https://github.com/kriswilk/themes/raw/refs/heads/main/base16/ekko-dark.yaml";
       sha256 = "sha256:0527d5jc4m817pdwkbpm8yz780pggwgylc26qx79akrdnqibnk5g";
+    };
+
+    fonts = {
+      monospace = {
+        name = "JetBrainsMonoNL Nerd Font";
+        package = pkgs.nerd-fonts.jetbrains-mono;
+      };
     };
   };
 }
