0ecb9821264fe37b47f107abbfd31b84253550cf
Kris Wilk <kris@reefnet.ca>
2025-12-05 13:29:45 -0500

    WIP

--- user/_modules/shell.nix
+++ user/_modules/shell.nix
@@ -20,8 +20,8 @@
         username.format = "[$user]($style)";
 
         hostname.ssh_only = false;
-        hostname.ssh_symbol = lib.mkForce " ";
-        hostname.format = "[$ssh_symbol](bold bright-white)[$hostname]($style) ";
+        hostname.ssh_symbol = lib.mkForce "|";
+        hostname.format = "[$ssh_symbol|](bold bright-white)[$hostname]($style) ";
         hostname.style = "bold green";
         
         directory.style = "bold blue";
