f273f8a0975faa63c65e19192560c4482be99453
Kris Wilk <kris@reefnet.ca>
2025-12-08 00:17:57 -0500

    WIP

--- user/_modules/font.nix
+++ user/_modules/font.nix
@@ -1,8 +1,14 @@
 { config, lib, pkgs, ... }:
 
 {
-  fonts = {
-    fontconfig.enable = true;
+  fonts.fontconfig = {
+    enable = true;
+    defaultFonts = {
+      sansSerif = [ "Noto Sans" ];
+      serif = [ "Noto Serif" ];
+      monospace = [ "JetBrainsMonoNL Nerd Font" ];
+      emoji = [ "Noto Color Emoji" ];
+    };
   };
   
   home.packages = with pkgs; [
