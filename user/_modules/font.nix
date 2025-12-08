2914325f7c7eb732091cbeddb4252712cdd93786
Kris Wilk <kris@reefnet.ca>
2025-12-08 08:37:17 -0500

    WIP

--- user/_modules/font.nix
+++ user/_modules/font.nix
@@ -4,4 +4,13 @@
   fonts.fontconfig = {
     enable = true;
   };
+
+  home.packages = with pkgs; [
+    # general purpose fonts
+    noto-fonts
+    noto-fonts-color-emoji
+    # programming
+    nerd.fonts.jetbrains-mono
+    nerd-fonts.symbols-only
+  ];
 }
