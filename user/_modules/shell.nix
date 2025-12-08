fd93c9415c8e81df5e4a89d31459864168f386b7
Kris Wilk <kris@reefnet.ca>
2025-12-08 13:25:25 -0500

    WIP

--- user/_modules/shell.nix
+++ user/_modules/shell.nix
@@ -19,20 +19,32 @@
       (builtins.fromTOML (builtins.readFile "${pkgs.starship}/share/starship/presets/no-empty-icons.toml"))
       {
         # WIP: other custom configuration here
-        username.show_always = true;
-        username.format = "[$user]($style) / ";
 
-        hostname.ssh_only = false;
-        hostname.ssh_symbol = lib.mkForce " ";
-        hostname.format = "[$ssh_symbol$hostname]($style) / ";
-        hostname.style = "bold green";
-        
-        directory.style = "bold blue";
+        format = "$sudo$username$hostname$directory$git_branch$git_commit$git_state$git_metrics$git_status$docker_context$package$fill$all";
 
-        cmd_duration.format = "󱎫 [$duration]($style) ";
+        fill = {
+          style = "bright-black";
+        };
 
-        format = "$username$hostname$localip$directory$git_branch$git_commit$git_state$git_metrics$git_status$docker_context$package$fill$all";
-        fill.style = "bright-black";
+        username = {
+          show_always = true;
+          format = "[$user]($style) / ";
+        };
+
+        hostname = {
+          ssh_only = false;
+          ssh_symbol = lib.mkForce " ";
+          format = "[$ssh_symbol$hostname]($style) / ";
+          style = "bold green";
+        };
+
+        directory = {
+          style = "bold blue";
+        };
+
+        cmd_duration = {
+          format = "󱎫 [$duration]($style) ";
+        };
       }
     ];
   };
