367217de0beef9085f6e59d8b3e7956eba52602b
Kris Wilk <kris@reefnet.ca>
2025-12-09 12:03:47 -0500

    WIP

--- user/_modules/terminal.nix
+++ user/_modules/terminal.nix
@@ -1,16 +1,16 @@
 { config, lib, pkgs, ... }:
 
 {
+  programs.foot = {
+    enable = true;
+  };
+
   programs.kitty = {
     enable = true;
     settings = {
       text_composition_strategy = "legacy";
       window_padding_width = 4;
     };
-  };
-
-  programs.foot = {
-    enable = true;
   };
 
   programs.tmux = {
