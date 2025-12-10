4d65c8d7b5bb62a56f5cd3d025429afe352eb963
Kris Wilk <kris@reefnet.ca>
2025-12-09 22:24:56 -0500

    WIP

--- user/_modules/shell.nix
+++ user/_modules/shell.nix
@@ -25,7 +25,7 @@ in {
   home.packages = with pkgs; [
     (blesh.overrideAttrs {
       version = "nightly-20251019+2f564e6";
-      src = lib.fetchzip {
+      src = fetchzip {
         url = "https://github.com/akinomyoga/ble.sh/releases/download/nightly/ble-nightly-20251019+2f564e6.tar.xz";
         sha256 = "sha256-fpNorzJcKs1vVhaYKgRz5vcs6jsEvdxe3N4F2L81Rc0=";
       };
