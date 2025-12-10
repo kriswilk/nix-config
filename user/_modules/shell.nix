86397c9a8d68ab0fb5354a77a080bec9cd700540
Kris Wilk <kris@reefnet.ca>
2025-12-10 13:48:35 -0500

    WIP

--- user/_modules/shell.nix
+++ user/_modules/shell.nix
@@ -3,16 +3,11 @@
 {
   programs.bash = {
     enable = true;
-    initExtra = ''
-      source $(blesh-share)/ble.sh
-    '';
   };
 
   programs.fish = {
     enable = true;
   };
-
-  home.file.".blerc".text = lib.readFile ./ble.sh/.blerc;
 
   programs.direnv = {
     enable = true;
@@ -22,30 +17,6 @@
     enable = true;
     settings = lib.importTOML ./starship/starship.toml;
   };
-
-  # home.packages = with pkgs; [
-  #   (blesh.overrideAttrs {
-  #     version = "nightly-20251019+2f564e6";
-  #     src = fetchzip {
-  #       url = "https://github.com/akinomyoga/ble.sh/releases/download/nightly/ble-nightly-20251019+2f564e6.tar.xz";
-  #       sha256 = "sha256-fpNorzJcKs1vVhaYKgRz5vcs6jsEvdxe3N4F2L81Rc0=";
-  #     };
-  #   })
-  # ];
-  # 131 
-  # 401 slim, not working
-  # 611 slim, not working
-  # 622 slim, working
-  # 701 no cursor but works
-  home.packages = with pkgs; [
-    (blesh.overrideAttrs {
-      version = "nightly-20240131+838b465";
-      src = fetchzip {
-        url = "https://github.com/akinomyoga/ble.sh/releases/download/nightly/ble-nightly-20240131+838b465.tar.xz";
-        sha256 = "sha256-d3dEM/tJ5eHZYxhRRTZaYIPMfyrVX3TAD1XLxN+VgOE=";
-      };
-    })
-  ];
 
   home.shellAliases = {
     # cd
