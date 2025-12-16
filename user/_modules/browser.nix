541f72cf1f2ff7594aba74c93f1960a57bc6e7c6
Kris Wilk <kris@reefnet.ca>
2025-12-15 23:23:18 -0500

    WIP

--- user/_modules/browser.nix
+++ user/_modules/browser.nix
@@ -4,41 +4,6 @@
   programs.firefox = {
     enable = true;
     profiles.default = {
-      search.engines = {
-        "Nix Packages" = {
-          definedAliases = [ "@np" ];
-          icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
-          urls = [{
-            template = "https://search.nixos.org/packages";
-            params = [
-              { name = "type"; value = "packages"; }
-              { name = "query"; value = "{searchTerms}"; }
-            ];
-          }];
-        };
-        "Nix Options" = {
-          definedAliases = [ "@no" ];
-          icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
-          urls = [{
-            template = "https://search.nixos.org/options";
-            params = [
-              { name = "type"; value = "options"; }
-              { name = "query"; value = "{searchTerms}"; }
-            ];
-          }];
-        };
-      };
-      search.force = true;
-
-      # bookmarks = [
-      #   {
-      #     name = "wikipedia";
-      #     tags = [ "wiki" ];
-      #     keyword = "wiki";
-      #     url = "https://en.wikipedia.org/wiki/Special:Search?search=%s&go=Go";
-      #   }
-      # ];
-
       settings = {
         #"dom.security.https_only_mode" = true;
         #"browser.download.panel.shown" = true;
@@ -50,22 +15,141 @@
       #   /* some css */                        
       # '';                                      
 
-      extensions.packages = with inputs.firefox-addons.packages."x86_64-linux"; [
-        bitwarden
-        ublock-origin
-        sponsorblock
-        #keepa???
-        #webedit???
-        #sinkitforreddit???
-        #clearurls???
-        #augmentedsteam???
-        #raindropio???
+      policies = {
+        AutofillAddressEnabled = false;
+        AutofillCreditCardEnabled = false;
+        DisableAppUpdate = true;
+        DisableFirefoxAccounts = true;
+        DisableFormHistory = true;
+        DisablePocket = true;
+        DisableTelemetry = true;
+        DisplayBookmarksToolbar = "never";
 
-        #darkreader
-        #tridactyl
-        #youtube-shorts-block
-      ];
+        ExtensionSettings = {
+          "*" = {
+            installation_mode = "blocked";
+          };
+          
+          # augmented steam          
+          "{1be309c5-3e4f-4b99-927d-bb500eb4fa88}" = {
+            install_url = "https://addons.mozilla.org/firefox/downloads/latest/augmented-steam/latest.xpi";
+            installation_mode = "force_installed";
+          };
 
+          # bitwarden
+          "{446900e4-71c2-419f-a6a7-df9c091e268b}" = {
+            install_url = "https://addons.mozilla.org/firefox/downloads/latest/bitwarden-password-manager/latest.xpi";
+            installation_mode = "force_installed";
+          };
+
+          # clearurls
+          "{74145f27-f039-47ce-a470-a662b129930a}" = {
+            install_url = "https://addons.mozilla.org/firefox/downloads/latest/clearurls/latest.xpi";
+            installation_mode = "force_installed";
+          };
+
+          # keepa
+          "amptra@keepa.com" = {
+            install_url = "https://addons.mozilla.org/firefox/downloads/latest/keepa/latest.xpi";
+            installation_mode = "force_installed";
+          };
+
+          # web edit
+          "webedit@ryanluu.dev" = {
+            install_url = "https://addons.mozilla.org/firefox/downloads/latest/web-edit/latest.xpi";
+            installation_mode = "force_installed";
+          };
+
+          # raindrop.io
+          "jid0-adyhmvsP91nUO8pRv0Mn2VKeB84@jetpack" = {
+            install_url = "https://addons.mozilla.org/firefox/downloads/latest/raindropio/latest.xpi";
+            installation_mode = "force_installed";
+          };
+
+          # sink it for reddit
+          "{09acf9ff-55d4-4366-a1a9-c9b3c8877c09}" = {
+            install_url = "https://addons.mozilla.org/firefox/downloads/latest/sink-it-for-reddit/latest.xpi";
+            installation_mode = "force_installed";
+          };
+
+          # sponsorblock
+          "sponsorBlocker@ajay.app" = {
+            install_url = "https://addons.mozilla.org/firefox/downloads/latest/sponsorblock/latest.xpi";
+            installation_mode = "force_installed";
+          };
+
+          # ublock-origin
+          "uBlock0@raymondhill.net" = {
+            install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
+            installation_mode = "force_installed";
+          };
+
+          # vimium
+          "{d7742d87-e61d-4b78-b8a1-b469842139fa}" = {
+            install_url = "https://addons.mozilla.org/firefox/downloads/latest/vimium-ff/latest.xpi";
+            installation_mode = "force_installed";
+          };
+        };
+
+        FirefoxHome = {
+          Search = true;
+          TopSites = false;
+          SponsoredTopSites = false;
+          Highlights = false;
+          Pocket = false;
+          Stories = false;
+          SponsoredPocket = false;
+          SpomoredStories = false;
+          Snippets = false;
+          Locked = true;
+        };
+
+        FirefoxSuggest = {
+          WebSuggestions = true;
+          SponsoredSuggestions = false;
+          ImproveSuggest = false;
+          Locked = true;
+        };
+
+        GenerativeAI = {
+          Enabled = false;
+          Chatbot = true;
+          LinkPreviews = false;
+          TabGroups = false;
+          Locked = true;
+        };
+
+        HttpsOnlyMode = "force_enabled";
+        NoDefaultBookmarks = true;
+        OfferToSaveLogins = false;
+        OverrideFirstRunPage = "";
+        OverridePostUpdatePage = "";
+        PasswordManagerEnabled = false;
+
+        Preferences = {
+          "browser.startup.homepage" = "about:newtab";
+        };
+
+        SearchEngines.Add = [
+          {
+            Name = "Nix Packages";
+            URLTemplate = "https://search.nixos.org/packages?query={searchTerms}";
+            IconURL = "https://nixos.org/favicon.ico";
+            Alias = "@np";
+          }
+          {
+            Name = "Nix Options";
+            URLTemplate = "https://search.nixos.org/options?query={searchTerms}";
+            IconURL = "https://nixos.org/favicon.ico";
+            Alias = "@no";
+          }
+        ];
+
+        ShowHomeButton = false;
+        SkipTermsOfUse = true;
+        TranslateEnabled = true;
+        VisualSearchEnabled = true;
+      };
     };
   };
 
