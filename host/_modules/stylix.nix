8c240d788d620706e36b2d80f3513edb878c9ce3
Kris Wilk <kris@reefnet.ca>
2025-12-19 14:05:08 -0500

    WIP

--- host/_modules/stylix.nix
+++ host/_modules/stylix.nix
@@ -3,7 +3,6 @@
 {
   stylix = {
     enable = true;
-    polarity = "light";
 
     base16Scheme = builtins.fetchurl {
       url = "https://github.com/kriswilk/themes/raw/refs/heads/main/ekko/base16/ekko-dark.yaml";
@@ -29,7 +28,14 @@
       emoji = {
         package = pkgs.noto-fonts-color-emoji;
         name = "Noto Color Emoji";
-      };      
+      };
+
+      sizes = {
+        applications = 10;
+        desktop = 10;
+        popups = 10;
+        terminal = 12;
+      };
     };
   };
 }
