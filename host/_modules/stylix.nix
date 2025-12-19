ada1f38f07d5284b5c2f2c069f426b468ea12a9d
Kris Wilk <kris@reefnet.ca>
2025-12-19 13:19:30 -0500

    WIP

--- host/_modules/stylix.nix
+++ host/_modules/stylix.nix
@@ -3,7 +3,7 @@
 {
   stylix = {
     enable = true;
-    target.kde.enable = false;
+    targets.kde.enable = false;
 
     base16Scheme = builtins.fetchurl {
       url = "https://github.com/kriswilk/themes/raw/refs/heads/main/ekko/base16/ekko-dark.yaml";
