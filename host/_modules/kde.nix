13d3d479762b6bf3a5b36022d4ceef7fa42c222c
Kris Wilk <kris@reefnet.ca>
2026-01-05 10:15:36 -0500

    WIP

--- host/_modules/kde.nix
+++ host/_modules/kde.nix
@@ -2,8 +2,13 @@
 
 {
   #services.displayManager.sddm.enable = true;
-  services.desktopManager.plasma6.enable = true;
+  #services.desktopManager.plasma6.enable = true;
 
   services.displayManager.ly.enable = true;
   programs.niri.enable = true;
+  environment.systemPackages = with pkgs; [
+    fuzzel
+    alacritty
+    swaylock
+  ];
 }
