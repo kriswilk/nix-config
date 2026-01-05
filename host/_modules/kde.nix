d765ac4b720ac38456f33169d971bfeac79f45d8
Kris Wilk <kris@reefnet.ca>
2026-01-05 09:14:43 -0500

    WIP

--- host/_modules/kde.nix
+++ host/_modules/kde.nix
@@ -1,6 +1,9 @@
 { config, lib, pkgs, ... }:
 
 {
-  services.displayManager.sddm.enable = true;
+  #services.displayManager.sddm.enable = true;
   services.desktopManager.plasma6.enable = true;
+
+  services.displayManager.ly.enable = true;
+  programs.niri.enable = true;
 }
