2aa475c337fbf774f89af5e05a0e078422c06af7
Kris Wilk <kris@reefnet.ca>
2026-01-05 12:39:34 -0500

    WIP

--- host/_modules/desktop.nix
+++ host/_modules/desktop.nix
@@ -1,17 +1,23 @@
 { config, lib, pkgs, ... }:
 
 {
-  services.displayManager.ly = {
-    enable = true;
-    settings = {
-      animation = "matrix";
-      bigclock = true;
-      hide_key_hints = true;
-      clear_password = true;
-      hide_version_string = true;
-    };
-  };
-  
+  # Enable the COSMIC login manager
+  services.displayManager.cosmic-greeter.enable = true;
+
+  # Enable the COSMIC desktop environment
+  services.desktopManager.cosmic.enable = true;
+
+  # services.displayManager.ly = {
+  #   enable = true;
+  #   settings = {
+  #     animation = "matrix";
+  #     bigclock = true;
+  #     hide_key_hints = true;
+  #     clear_password = true;
+  #     hide_version_string = true;
+  #   };
+  # };
+
   services.desktopManager.plasma6.enable = true;
 
   programs.niri.enable = true;
