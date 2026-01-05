bbf6c4887ac427bdb55d647c187c5814d154bc85
Kris Wilk <kris@reefnet.ca>
2026-01-05 11:09:34 -0500

    WIP

--- host/_modules/desktop.nix
+++ host/_modules/desktop.nix
@@ -1,15 +1,16 @@
 { config, lib, pkgs, ... }:
 
 {
-  #services.displayManager.sddm.enable = true;
+  services.displayManager.sddm.enable = true;
   services.desktopManager.plasma6.enable = true;
 
-  services.displayManager.ly.enable = true;
   programs.niri.enable = true;
+  
   environment.systemPackages = with pkgs; [
     fuzzel
     alacritty
     swaylock
     xwayland-satellite
+    waybar
   ];
 }
