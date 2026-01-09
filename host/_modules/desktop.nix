a05549545a1df2af3949f36c3c84630663957659
Kris Wilk <kris@reefnet.ca>
2026-01-09 08:49:08 -0500

    WIP

--- host/_modules/desktop.nix
+++ host/_modules/desktop.nix
@@ -19,10 +19,10 @@
 
   # WIP: Niri needs a lot more configuration to be usable...
   programs.niri.enable = true;
-  #programs.dms-shell.enable = true;
+  programs.dms-shell.enable = true;
   
   environment.systemPackages = with pkgs; [
-    noctalia-shell
+    #noctalia-shell
     fuzzel
     mako
     alacritty
