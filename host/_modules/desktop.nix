2c751c60c2fa1b23a90ed92047d6e3eb5265438b
Kris Wilk <kris@reefnet.ca>
2026-01-10 22:15:58 -0500

    WIP

--- host/_modules/desktop.nix
+++ host/_modules/desktop.nix
@@ -8,10 +8,10 @@
       default_session = {
         command = ''
           ${pkgs.tuigreet}/bin/tuigreet \
-          --cmd niri-session \
           --time --time-format '%a, %b %d %Y • %T' \
           --width 50 --window-padding 1 --container-padding 2 \
-          --theme 'container=black;border=blue;title=yellow;greet=blue;text=gray;prompt=White;input=gray;time=white;action=blue;button=yellow' \
+          --greeting 'Welcome to NixOS!' \
+          --theme 'container=black;border=black;title=black;greet=blue;text=gray;prompt=White;input=gray;time=white;action=blue;button=yellow' \
           --asterisks \
         '';
         user = "greeter";
