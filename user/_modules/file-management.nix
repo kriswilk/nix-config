cc61f885e7fe0d8a2c67815b155d281ef2b2934d
Kris Wilk <kris@reefnet.ca>
2026-01-15 11:30:36 -0500

    WIP

--- user/_modules/file-management.nix
+++ user/_modules/file-management.nix
@@ -17,7 +17,7 @@
     keymap = {
       mgr.prepend_keymap = [
         {
-          on = "l";
+          on = "<Enter>";
           run = "plugin smart-enter";
           desc = "Enter the child directory, or open the file";
         }
