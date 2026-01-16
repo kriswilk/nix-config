d0faab8fb00fe7eab56f43db40a88b20a68a801a
Kris Wilk <kris@reefnet.ca>
2026-01-16 10:48:21 -0500

    WIP

--- host/_modules/desktop.nix
+++ host/_modules/desktop.nix
@@ -18,23 +18,6 @@
     };
   };
 
-  # services.displayManager.ly = {
-  #   enable = true;
-  #   settings = {
-  #     animation = "matrix";
-  #     #hide_key_hints = true;
-  #     #hide_version_string = true;
-  #     input_len = 24;
-  #     text_in_center = true;
-  #     clear_password = true;
-  #     save = true;
-  #   };
-  # };
-
-  # WIP: to be replaced by niri or hyprland?
-  #services.desktopManager.plasma6.enable = true;
-
   # WIP: Niri needs a lot more configuration to be usable...
   programs.niri.enable = true;
-  #programs.dms-shell.enable = true;
 }
