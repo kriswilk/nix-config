d53d955077c9a8d355b2eb707d86b8d3923fb09b
Kris Wilk <kris@reefnet.ca>
2026-01-16 10:55:06 -0500

    WIP

--- user/_scripts/menu-power.sh
+++ user/_scripts/menu-power.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 SELECTION="$(printf "  Lock\n󰤄  Suspend\n󰜎  Log Out\n  Reboot\n  Shutdown" | fuzzel --dmenu -l 5 -w 13 --line-height "45" --vertical-pad 20 --hide-prompt -f "monospace:size=20")"
 
