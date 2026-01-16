ebe41a5158968381c0f1e06dcffbe412057f468b
Kris Wilk <kris@reefnet.ca>
2026-01-15 23:43:50 -0500

    WIP

--- user/_modules/desktop.nix
+++ user/_modules/desktop.nix
@@ -40,20 +40,17 @@
       #{ timeout = 90; command = "systemctl suspend"; }
     ];
     events = {
-      before-sleep = "${pkgs.swaylock-effects}/bin/swaylock --daemonize --screenshots --clock --indicator";
+      before-sleep = "${pkgs.swaylock}/bin/swaylock -f";
     };
   };
 
   programs.swaylock = {
     enable = true;
-    package = pkgs.swaylock-effects;
     settings = {
+      image = ../_assets/padlock.png
       font-size = 30;
       indicator-radius = 150;
       indicator-thickness = 7;
-      effect-blur = "7x5";
-      effect-vignette = "0.5:0.5";
-      fade-in = 0.2;
       show-failed-attempts = true;
     };
   };
