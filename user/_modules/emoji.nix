9123edf79f5e9c77724f11aba67470c267078ee7
Kris Wilk <kris@reefnet.ca>
2026-01-22 10:15:52 -0500

    WIP

--- user/_modules/emoji.nix
+++ user/_modules/emoji.nix
@@ -6,13 +6,13 @@
     #BEMOJI_HISTORY_LOCATION="$XDG_STATE_HOME"; # where the state file resides
     #BEMOJI_CUSTOM_LIST=""; # the custom emoji list to display
     BEMOJI_DOWNLOAD_LIST="all"; # the default emoji lists to download to database
-    #BEMOJI_DEFAULT_COMMAND=""; # which command to invoke by default
+    BEMOJI_DEFAULT_COMMAND="fuzzel -d"; # which command to invoke by default
     #BEMOJI_PICKER_CMD="bemenu"; # which picker tool to use
     #BEMOJI_CLIP_CMD="wl-copy"; # which clipboard tool to use
     #BEMOJI_TYPE_CMD="wtype"; # which typing tool to use (ydotool will NOT work)
-    #BEMOJI_PRIVATE_MODE=false; # whether to save new entries
-    #BEMOJI_LIMIT_RECENT=""; # whether to display recent entries
-    BEMOJI_ECHO_NEWLINE=fals; # whether to end the output with a newline character
+    BEMOJI_PRIVATE_MODE=true; # whether to save new entries
+    BEMOJI_LIMIT_RECENT=0; # whether to display recent entries
+    BEMOJI_ECHO_NEWLINE=false; # whether to end the output with a newline character
   };
 
   home.packages = with pkgs; [
--- user/_scripts/menu-power.sh
+++ user/_scripts/menu-power.sh
@@ -1,6 +1,9 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
-SELECTION="$(printf "  Lock\n󰤄  Suspend\n󰜎  Log Out\n  Reboot\n  Shutdown" | fuzzel --dmenu -l 5 -w 13 --line-height "45" --vertical-pad 20 --hide-prompt -f "monospace:size=20")"
+CHOICES="   Lock\n 󰤄  Suspend\n 󰜎  Log Out\n   Reboot\n   Shutdown"
+DMENU="fuzzel --dmenu -l 5 -w 13 --line-height "45" --vertical-pad 20 --hide-prompt -f "monospace:size=20""
+
+SELECTION="$(printf $CHOICES | $DMENU)"
 
 case $SELECTION in
 	*"Lock")
