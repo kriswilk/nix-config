79cb36231bec082f9aa6a673e89ad6908742feda
Kris Wilk <kris@reefnet.ca>
2026-01-22 11:21:06 -0500

    WIP

--- user/_scripts/menu-power.sh
+++ user/_scripts/menu-power.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 CHOICES="   Lock\n 󰤄  Suspend\n 󰜎  Log Out\n   Reboot\n   Shutdown"
-DMENU="fuzzel --dmenu -l 5 -w 13 --line-height "45" --vertical-pad 20 --hide-prompt -f "monospace:size=20""
+DMENU="fuzzel --dmenu -l 5 -w 13 --line-height 45 --vertical-pad 20 --hide-prompt -f monospace:size=20"
 
-SELECTION="$(printf $CHOICES | $DMENU)"
+SELECTION="$(printf "$CHOICES" | $DMENU)"
 
 case $SELECTION in
 	*"Lock")
