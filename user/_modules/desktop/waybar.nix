f63317969cfdfdc695c95dc2069e7ec6367cfe27
Kris Wilk <kris@reefnet.ca>
2026-01-23 13:50:18 -0500

    WIP

--- user/_modules/desktop/waybar.nix
+++ user/_modules/desktop/waybar.nix
@@ -3,8 +3,8 @@
 {
   programs.waybar = {
     enable = true;
-    style = builtins.readFile ../_dotfiles/waybar/style.css;
+    style = builtins.readFile ../../_dotfiles/waybar/style.css;
   };
-  xdg.configFile."waybar/config".source = ../_dotfiles/waybar/config.jsonc;
-  #xdg.configFile."waybar/style.css".source = ../_dotfiles/waybar/style.css;
+  xdg.configFile."waybar/config".source = ../../_dotfiles/waybar/config.jsonc;
+  #xdg.configFile."waybar/style.css".source = ../../_dotfiles/waybar/style.css;
 }
