3a5a05cc17e1ee134b7c33a0bec081960938493a
Kris Wilk <kris@reefnet.ca>
2026-01-23 14:37:12 -0500

    WIP

--- user/_modules/desktop/swaylock.nix
+++ user/_modules/desktop/swaylock.nix
@@ -9,9 +9,28 @@
       font-size = 36;
       indicator-radius = 256;
       indicator-thickness = 18;
+
+      # transparent
       inside-color = lib.mkForce "${config.lib.stylix.colors.base00}00";
+
+      # clear - blue ring, everything else transparent
+      ring-clear-color = lib.mkForce "${config.lib.stylix.colors.base0D}FF";
       inside-clear-color = lib.mkForce "${config.lib.stylix.colors.base00}00";
-      ring-clear-color = lib.mkForce "${config.lib.stylix.colors.base0D}00";
+      text-clear-color = lib.mkForce "${config.lib.stylix.colors.base00}00";
+
+      # verify - yellow ring, everything else transparent
+      ring-ver-color = lib.mkForce "${config.lib.stylix.colors.base0A}FF";
+      inside-ver-color = lib.mkForce "${config.lib.stylix.colors.base00}00";
+      text-ver-color = lib.mkForce "${config.lib.stylix.colors.base00}00";
+
+      # wrong - red ring, everything else transparent
+      ring-wrong-color = lib.mkForce "${config.lib.stylix.colors.base08}FF";
+      inside-wrong-color = lib.mkForce "${config.lib.stylix.colors.base00}00";
+      text-wrong-color = lib.mkForce "${config.lib.stylix.colors.base00}00";
+
+      # caps lock - purple ring, everything else transparent
+      line-caps-lock-color = lib.mkForce "${config.lib.stylix.colors.base0F}FF";
+      text-caps-lock-color = lib.mkForce "${config.lib.stylix.colors.base00}00";
     };
   };
 }
