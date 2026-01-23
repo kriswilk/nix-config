5851cb2a159b037ae4c5b8f523ea94b57614e408
Kris Wilk <kris@reefnet.ca>
2026-01-23 17:02:54 -0500

    WIP

--- user/_modules/desktop/swaylock.nix
+++ user/_modules/desktop/swaylock.nix
@@ -6,30 +6,40 @@
     settings = {
       image = ./swaylock.png;
       scaling = lib.mkForce "center";
+      font = "monospace";
       font-size = 36;
       indicator-radius = 256;
       indicator-thickness = 18;
 
-      # # transparent inside (to see lock icon)
-      # inside-color = lib.mkForce "${config.lib.stylix.colors.base00}00";
-      # # ...
-      # inside-caps-lock-color = lib.mkForce "${config.lib.stylix.colors.base00}00";
-      # inside-clear-color = lib.mkForce "${config.lib.stylix.colors.base00}00";
-      # inside-ver-color = lib.mkForce "${config.lib.stylix.colors.base00}00";
-      # inside-wrong-color = lib.mkForce "${config.lib.stylix.colors.base00}00";
-      # # ...
-      # text-caps-lock-color = lib.mkForce "${config.lib.stylix.colors.base00}00";
-      # text-clear-color = lib.mkForce "${config.lib.stylix.colors.base00}00";
-      # text-ver-color = lib.mkForce "${config.lib.stylix.colors.base00}00";
-      # text-wrong-color = lib.mkForce "${config.lib.stylix.colors.base00}00";
+      # keypresses
+      bs-hl-color = lib.mkForce "${config.lib.stylix.colors.base08}"; # red
+      key-hl-color = lib.mkForce "${config.lib.stylix.colors.base0B}"; # green
+      # ...
+      caps-lock-bs-hl-color = lib.mkForce "${config.lib.stylix.colors.base08}"; # red
+      caps-lock-key-hl-color = lib.mkForce "${config.lib.stylix.colors.base0A}"; # yellow
 
-      # # clear (blue), verify (yellow), wrong (red)
-      # ring-clear-color = lib.mkForce "${config.lib.stylix.colors.base0D}FF";
-      # ring-ver-color = lib.mkForce "${config.lib.stylix.colors.base0A}FF";
-      # ring-wrong-color = lib.mkForce "${config.lib.stylix.colors.base08}FF";
+      inside-color = lib.mkForce "00000000";
+      # ...
+      inside-caps-lock-color = lib.mkForce "${config.lib.stylix.colors.base0A}88";
+      inside-clear-color = lib.mkForce "00000000";
+      inside-ver-color = lib.mkForce "00000000";
+      inside-wrong-color = lib.mkForce "00000000";
 
-      # # caps-lock warning (orange)
-      # caps-lock-key-hl-color = lib.mkForce "${config.lib.stylix.colors.base0A}FF";
+      text-color = lib.mkForce "FFFFFF";
+      # ...
+      text-caps-lock-color = lib.mkForce "FFFFFF";
+      text-clear-color = lib.mkForce "FFFFFF";
+      text-ver-color = lib.mkForce "FFFFFF";
+      text-wrong-color = lib.mkForce "FFFFFF";
+
+      ring-color = lib.mkForce "${config.lib.stylix.colors.base05}AA"; # light gray
+      # ...
+      ring-caps-lock-color = lib.mkForce "${config.lib.stylix.colors.base05}FF"; # light gray
+      ring-clear-color = lib.mkForce "${config.lib.stylix.colors.base0D}FF"; # blue
+      ring-ver-color = lib.mkForce "${config.lib.stylix.colors.base0A}FF"; # yellow
+      ring-wrong-color = lib.mkForce "${config.lib.stylix.colors.base08}FF"; # red
+
+      separator-color = lib.mkForce "00000000";
     };
   };
 }
