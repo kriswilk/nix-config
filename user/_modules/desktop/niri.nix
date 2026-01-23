56121d2d88caed12b982450ffaa41860184c03db
Kris Wilk <kris@reefnet.ca>
2026-01-23 13:50:48 -0500

    WIP

--- user/_modules/desktop/niri.nix
+++ user/_modules/desktop/niri.nix
@@ -1,7 +1,7 @@
 { config, lib, pkgs, ... }:
 
 {
-  xdg.configFile."niri/config.kdl".source = ../_dotfiles/niri/config.kdl;
+  xdg.configFile."niri/config.kdl".source = ../../_dotfiles/niri/config.kdl;
 
   home.packages = with pkgs; [
     xwayland-satellite
