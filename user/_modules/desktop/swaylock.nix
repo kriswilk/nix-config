d293d926de78b84d7943e1b6a731264346549cba
Kris Wilk <kris@reefnet.ca>
2026-01-23 13:56:27 -0500

    WIP

--- user/_modules/desktop/swaylock.nix
+++ user/_modules/desktop/swaylock.nix
@@ -7,7 +7,7 @@
       image = ./swaylock.png;
       scaling = lib.mkForce "center";
       font-size = 36;
-      indicator-radius = 200;
+      indicator-radius = 128;
       indicator-thickness = 20;
       inside-color = lib.mkForce "${config.lib.stylix.colors.base00}00";
     };
