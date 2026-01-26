281fc15aeb612a6326e1edc4e3ee59865d28935d
Kris Wilk <kris@reefnet.ca>
2026-01-26 10:07:58 -0500

    WIP

--- host/_modules/nixos.nix
+++ host/_modules/nixos.nix
@@ -3,7 +3,20 @@
 {
   system.stateVersion = "25.11";
 
-  nix.settings.experimental-features = [ "flakes" "nix-command" ];  
+  nix.settings = {
+    experimental-features = [
+      "flakes"
+      "nix-command"
+    ];
+    extra-substituters = [
+      "https://walker.cachix.org"
+      "https://walker-git.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+      "walker.cachix.org-1:fG8q+uAaMqhsMxWjwvk0IMb4mFPFLqHjuvfwQxE4oJM="
+      "walker-git.cachix.org-1:vmC0ocfPWh0S/vRAQGtChuiZBTAe4wiKDeyyXM0/7pM="
+    ];
+  };
 
   nixpkgs.config.allowUnfree = true;
 
