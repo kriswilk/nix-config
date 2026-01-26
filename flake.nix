a8084440952e8831abb424bee66ffdf4a792f377
Kris Wilk <kris@reefnet.ca>
2026-01-26 10:05:07 -0500

    WIP

--- flake.nix
+++ flake.nix
@@ -2,29 +2,26 @@
   description = "NixOS Host & User Configuration";
   
   inputs = {
-    nixpkgs = {
-      url = "github:NixOS/nixpkgs/nixos-unstable";
-    };
-    disko = {
-      url = "github:nix-community/disko";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    stylix = {
-      url = "github:nix-community/stylix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    nix-index-database = {
-      url = "github:nix-community/nix-index-database";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    nvf = {
-      url = "github:notashelf/nvf";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    
+    disko.url = "github:nix-community/disko";
+    disko.inputs.nixpkgs.follows = "nixpkgs";
+    
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+
+    stylix.url = "github:nix-community/stylix";
+    stylix.inputs.nixpkgs.follows = "nixpkgs";
+
+    nix-index-database.url = "github:nix-community/nix-index-database";
+    nix-index-database.inputs.nixpkgs.follows = "nixpkgs";
+
+    nvf.url = "github:notashelf/nvf";
+    nvf.inputs.nixpkgs.follows = "nixpkgs";
+
+    elephant.url = "github:abenz1267/elephant";
+    walker.url = "github:abenz1267/walker";
+    walker.inputs.elephant.follows = "elephant";
   };
 
   outputs = { nixpkgs, ... }@inputs:
@@ -43,8 +40,9 @@
             useGlobalPkgs = true;
             useUserPackages = true;
             sharedModules = [
-              inputs.nvf.homeManagerModules.default
               inputs.nix-index-database.homeModules.default
+              inputs.nvf.homeManagerModules.default
+              inputs.walker.homeManagerModules.default
             ];
             extraSpecialArgs = { inherit inputs; };
           };
